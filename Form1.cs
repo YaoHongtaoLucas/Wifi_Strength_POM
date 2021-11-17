d325408ec79de6716517828fa8df182315f5ffe8
YaoHongtaoLucas <94575820+YaoHongtaoLucas@users.noreply.github.com>
2021-11-17 18:19:29 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NativeWifi;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void SSID()
+        {
+            WlanClient client = new WlanClient();
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+                foreach (Wlan.WlanAvailableNetwork network in networks)
+                {
+                    Console.WriteLine("Found network with SSID {0} and Siqnal Quality {1}.", GetStringForSSID(network.dot11Ssid), network.wlanSignalQuality);
+                }
+            }
+        }
+
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+        {
+            return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SSID();
+        }
+    }
+}
+
+
+
+
+
